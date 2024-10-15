f82653aae34ef1cd653cbf6d9441ae8c9bfbcfd4
Connor Fricke <cd.fricke23@gmail.com>
2024-10-15 17:41:56 -0400

    first commit, nothing here yet

---
+++ main.py
@@ -0,0 +1,10 @@
+#
+# Programmer: Connor Fricke (fricke.59@osu.edu)
+# File: main.py
+# Latest Revision: 8-Oct-2024
+# Desc: Main python script for PHYSICS 5680 - Big Data Analytics & Machine Learning final project
+#
+
+import numpy as np
+import pandas as pd
+import plotly_express as px
